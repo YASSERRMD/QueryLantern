d48bc6d052f148aee26efdbd3bddfc10e77a6e3c
YASSERRMD <arafath.yasser@gmail.com>
2026-07-17 20:22:26 +0400

    feat(confidence): show confidence band and not-sure disclaimer in the UI
    Render ConfidenceView after the critique card and register ConfidenceService so
    low-confidence answers display an 'I am not sure' warning.

--- QueryLantern/Program.cs
+++ QueryLantern/Program.cs
@@ -56,6 +56,7 @@ builder.Services.AddScoped<QueryLantern.Services.GraphRunService>();
 builder.Services.AddScoped<QueryLantern.Services.OrchestrationService>();
 builder.Services.AddScoped<QueryLantern.Services.AnswerGroundingService>();
 builder.Services.AddScoped<QueryLantern.Services.AnswerCriticService>();
+builder.Services.AddScoped<QueryLantern.Services.ConfidenceService>();
 builder.Services.AddScoped<QueryLantern.Services.DecompositionService>();
 builder.Services.AddScoped<QueryLantern.Services.PlanService>();
 builder.Services.AddScoped<ChatService>();
